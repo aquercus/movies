1efb5c560e05f61ccff4487fa58f3b5f94b5e803
Helen <aquercus@gmail.com>
2021-01-20 22:54:45 -0600

    get and add methods added

--- src/main/java/org/aquercus/movie/MovieService.java
+++ src/main/java/org/aquercus/movie/MovieService.java
@@ -2,8 +2,7 @@ package org.aquercus.movie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -18,12 +17,30 @@ public class MovieService {
         return "Hello!";
     }
 
-    @GetMapping("/list")
+    @GetMapping("/movies")
     public Collection<MovieModel> list(){
         return jdbcTemplate.query(
                 "select imdb_id, title, year, rating from movies",
                 (rs, rowNum) -> new MovieModel(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getBigDecimal(4))
         );
 
+    }
+
+    @GetMapping("/movies/{id}")
+    public MovieModel get(@PathVariable("id") String id){
+        return jdbcTemplate.queryForObject(
+                "select imdb_id, title, year, rating from movies where imdb_id = ?",
+                (rs, rowNum) -> new MovieModel(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getBigDecimal(4)),
+                id
+        );
+    }
+
+    @PostMapping("/movies")
+    public MovieModel add(@RequestBody MovieModel movie){
+        jdbcTemplate.update(
+                "insert into movies (imdb_id, title, year, rating) values (?,?,?,?)",
+                movie.getImdbId(), movie.getTitle(), movie.getYear(), movie.getRating()
+                );
+        return get(movie.getImdbId());
     }
 }
