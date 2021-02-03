016963b19dd7975c2debcc999e6a3b6ca9570f31
Helen <aquercus@gmail.com>
2021-02-02 21:34:12 -0600

    disabled CORS

--- src/main/java/org/aquercus/movie/MovieApplication.java
+++ src/main/java/org/aquercus/movie/MovieApplication.java
@@ -2,6 +2,12 @@ package org.aquercus.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MovieApplication {
@@ -10,4 +16,47 @@ public class MovieApplication {
         SpringApplication.run(MovieApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer resourceResolver(){
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .exposedHeaders(
+                                "Access-Control-Allow-Origin",
+                                "Access-Control-Allow-Methods",
+                                "Access-Control-Allow-Headers",
+                                "Access-Control-Max-Age",
+                                "Access-Control-Request-Header",
+                                "Access-Control-Request-Method"
+                        );
+            }
+        };
+    }
+
+    @Bean
+    public WebSecurityConfigurerAdapter securifier(){
+        return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                http.anonymous()
+                        .and()
+                        .authorizeRequests()
+                        .antMatchers("/**")
+                        .permitAll()
+                        .antMatchers(HttpMethod.OPTIONS,"/**")
+                        .permitAll()
+                        .and()
+                        .authorizeRequests()
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .csrf().disable();
+                           }
+        };
+    }
 }
